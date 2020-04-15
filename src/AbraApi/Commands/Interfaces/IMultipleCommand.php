9735f8a6c984f81e1290033a3332a0a244b58999
Lukáš Křehula <lukas@krehula.net>
2020-04-15 11:46:01 +0200

    Added new common interface to specify command, which can be specified multiple times in query

---
+++ src/AbraApi/Commands/Interfaces/IMultipleCommand.php
@@ -0,0 +1,10 @@
+<?php 
+
+	namespace AbraApi\Commands\Interfaces;
+
+	/**
+	 * Interface that specifies, if command can be specified multiple times in query
+	 */
+	interface IMultipleCommand {
+		
+	}
